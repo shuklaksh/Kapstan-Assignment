1bc3a18da1e6ee5a1227b079ffed2a45fa37c7b9
shuklaksh <shuklaksh156@gmail.com>
2024-05-02 00:27:39 +0530

    feat: added Sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -112,9 +112,20 @@ const SidebarItems = [
     },
 ]
 
+const SidebarItems2 = [
+    {
+        label:  'Admin',
+        icon:   < InboxIcon />
+    },
+    {
+        label:  'Docs',
+        icon:   < InboxIcon />
+    },
+]
+
 export default function Sidebar() {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -147,11 +158,14 @@ export default function Sidebar() {
         </Toolbar>
       </AppBar>
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-        <Typography variant="h6" noWrap component="div">
-            Kapstan
-          </Typography>
-          
+        <DrawerHeader sx={{justifyContent: "start"}}>
+        <svg width="35" height="32" viewBox="0 0 35 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M28.7848 3.99267C27.3574 1.52397 24.7198 0 21.8683 0H12.6176C9.76274 0 7.1251 1.52397 5.70111 3.99267L1.07057 12.0056C-0.356858 14.4778 -0.356858 17.5223 1.07057 19.9944L5.69766 28.0073C7.1251 30.4795 9.76274 32 12.6142 32H21.8649C24.7198 32 27.3574 30.476 28.7814 28.0073L33.4085 19.9944C34.8359 17.5223 34.8359 14.4778 33.4085 12.0056L28.7814 3.99267H28.7848ZM31.0984 23.1734C31.0984 23.6837 30.826 24.156 30.3847 24.4077L17.955 31.5828C17.5136 31.838 16.9689 31.838 16.5275 31.5828L4.09784 24.4112C3.6565 24.156 3.38412 23.6837 3.38412 23.1768V8.82319C3.38412 8.3129 3.6565 7.84053 4.09784 7.58884L16.5275 0.410301C16.9689 0.155156 17.5136 0.155156 17.955 0.410301L30.3847 7.58539C30.826 7.84054 31.0984 8.3129 31.0984 8.81974V23.1734Z" fill="white"/>
+        </svg>
+
+            <Typography variant="h6" noWrap component="div" >
+                Kapstan
+            </Typography>
         </DrawerHeader>
         <Divider />
         <List>
@@ -179,64 +193,37 @@ export default function Sidebar() {
           ))}
         </List>
         <Divider />
-        {/* <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
+        <Box sx={{height: "100%",display: "flex", flexDirection: "column", justifyContent: "flex-end"}}>
+            <List sx={{alignItems: "flex-end"}}>
+            {SidebarItems2?.map((item, index) => (
+                <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+                <ListItemButton
+                    sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
+                    }}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
-        <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box>
+                    <ListItemIcon
+                    sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : 'auto',
+                        justifyContent: 'center',
+                    }}
+                    >
+    {item?.icon}
+                    
+                    </ListItemIcon>
+                    <ListItemText primary={item.label} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+                </ListItem>
+            ))}
+            </List>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+        </Box>
+      </Drawer> 
     </Box>
   );
 }
